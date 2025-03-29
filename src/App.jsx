f2e6318a9f645a1d2ebb2024a26acfbe549f7feb
Wyco <herik.dev06@gmail.com>
2025-03-29 18:43:41 +0700

    router set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import HomeCards from "./components/HomeCards";
-import JobListings from "./components/JobListings";
-import ViewAllJob from "./components/ViewAllJob";
+import { Route,RouterProvider, createBrowserRouter,createRoutesFromElements } from "react-router-dom";
+
+import MainLayout from "./Pages/MainLayout";
+import HomePage from "./Pages/HomePage";
+import JobPage from "./Pages/JobPage";
+import NotFoundPage from "./Pages/NotFoundPage";
+
+const router= createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<MainLayout/>}>
+      <Route index element={<HomePage/>}/>
+      <Route path="/jobs" element={<JobPage/>}/>
+      <Route path= "*" element={<NotFoundPage/>}/>
+    </Route>
+  )
+)
 
 const App = () => {
-  return (
-    <>
-      <Navbar />
-      <Hero/>
-      <HomeCards />
-      <JobListings />
-      <ViewAllJob />
-    </>
-  );
+  return <RouterProvider router={router}/>;
 };
 
 export default App;
