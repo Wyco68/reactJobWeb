9402650daaa4b8d59f5797ff0610eb18d1b9efc8
Wyco <herik.dev06@gmail.com>
2025-03-30 14:17:52 +0700

    card

--- src/components/HomeCards.jsx
+++ src/components/HomeCards.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import Card from "./Card.jsx"
 
 const HomeCards = () => {
   return (
     <section className="py-4">
       <div className="container-xl lg:container m-auto">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4 rounded-lg">
+          <Card>
           <div className="bg-gray-100 p-6 rounded-lg shadow-md">
             <h2 className="text-2xl font-bold">For Developers</h2>
             <p className="mt-2 mb-4">
@@ -17,6 +18,8 @@ const HomeCards = () => {
               Browse Jobs
             </a>
           </div>
+          </Card>
+          <Card bg={'bg-indigo-100'}>
           <div className="bg-indigo-100 p-6 rounded-lg shadow-md">
             <h2 className="text-2xl font-bold">For Employers</h2>
             <p className="mt-2 mb-4">
@@ -29,6 +32,7 @@ const HomeCards = () => {
               Add Job
             </a>
           </div>
+          </Card>
         </div>
       </div>
     </section>
